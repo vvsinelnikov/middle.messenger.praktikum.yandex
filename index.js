241fdcd7f9b7e8c131da979a27a57d0a7479af60
Vladimir Sinelnikov <vvsinelnikov@yandex.ru>
2022-01-01 14:17:54 +0300

    первичная настройка done

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-// import './src/pages/home/modules/login/index.js';
+const express = require('express');
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static('./dist/'));
+
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`);
+});
--- src/pages/home/modules/login/index.js
+++ src/pages/home/modules/login/index.js
@@ -1,2 +1,28 @@
 import './login.css';
 
+const Handlebars = require("handlebars");
+
+const template = `
+    <div class="page">
+        <form class="login__form">
+            <div class="login_container">
+                <h1 class="login__heading">Вход</h1>
+                <label htmlFor="login" class="login__descriptor login__descriptor__hidden">Логин</label>
+                <input id="login" class="login__input" type='text' placeholder="Логин" required minLength='2'
+                       maxLength='40'/>
+                <span class="login__error login__error__hidden">Ошибка</span>
+                <label htmlFor="password" class="login__descriptor login__descriptor__hidden">Пароль</label>
+                <input id="password" class="login__input" type='password' placeholder="Пароль" required minLength='2'
+                       maxLength='40'/>
+                <span class="login__error login__error__hidden">Ошибка</span>
+            </div>
+            <div class="login_container">
+                <input class="login__button" type="submit" value="Авторизоваться"/>
+                <a class="login__link" href="/signin">Нет аккаунта?</a>
+            </div>
+        </form>
+    </div>
+`;
+
+const login = Handlebars.compile(template)({});
+document.body.innerHTML = login
